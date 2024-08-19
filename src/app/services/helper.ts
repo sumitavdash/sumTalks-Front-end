9fe0bb9993fb2c7e93ad5f2f92fe3cd6a199fb05
sumitav dash <devsumitav@gmail.com>
2024-08-19 20:40:03 +0530

    ng-rock-added

--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,4 +1,4 @@
 // let baseUrl="http://localhost:4000"
 let baseUrl =
-  'https://aa80-2401-4900-3dab-7435-2d75-b6dc-e83c-f454.ngrok-free.app';
+  'https://b05f-2401-4900-3dab-7435-2d75-b6dc-e83c-f454.ngrok-free.app';
 export default baseUrl;
