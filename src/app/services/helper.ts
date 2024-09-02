fe2c640399d02aa3d442b730d57df4e1faabe908
sumitav dash <devsumitav@gmail.com>
2024-09-03 01:55:01 +0530

    ngrok-change

--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,4 +1,4 @@
 // let baseUrl = 'http://localhost:4000';
 let baseUrl =
-  'https://0269-2401-4900-7019-391c-58b1-cdaf-3899-c07b.ngrok-free.app';
+  'https://bd7a-2409-40e2-2020-ff9d-da7-e109-d62f-f6a1.ngrok-free.app';
 export default baseUrl;
